f3a246a62b7ddc395200b2a0d3a83dcd6eb5d726
Crowdbotics <team@crowdbotics.com>
2023-11-21 00:10:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-frog-44694",
+  url: "https://misty-frog-44694.botics.co",
+  api: "https://misty-frog-44694.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
